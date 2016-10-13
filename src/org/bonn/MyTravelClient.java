97f513e734469378f9ada48717cc55af126f9901
aCrylic <aCrylic@DESKTOP-QJ3AECQ>
2016-10-14 00:34:30 +0200

    new commit

---
+++ src/org/bonn/MyTravelClient.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.bonn;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import org.bonn.ooka.ejb.MyTravelEJBRemote;
+import org.bonn.ooka.ejb.TravelData;
+
+/**
+ *
+ * @author aCrylic
+ */
+public class MyTravelClient {
+
+    private MyTravelEJBRemote travel;
+    
+    public void run() {
+        try {
+            travel = (MyTravelEJBRemote) new InitialContext().lookup (
+                    "java:global/MyTravelApplication/MyTravelApplication-ejb/MyTravelEJB"
+                            + "!org.bonn.ooka.ejb.MyTravelEJBRemote"
+            );
+        } catch (NamingException ex) {
+            Logger.getLogger(MyTravelClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        travel.checkName("alda");    
+        TravelData data = new TravelData();
+        data.setLocation("Bonn");
+        String str = travel.bookTravel(data);
+        System.out.println("Ergebnis: " + str);
+        System.out.println("Location: " + data.getLocation());
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MyTravelClient c = new MyTravelClient();
+        c.run();
+    }
+    
+}
